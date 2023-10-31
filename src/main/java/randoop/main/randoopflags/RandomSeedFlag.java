4ee2404e58792d373cb5c9ee117dc113757f8da2
AFungo <augustofungo@gmail.com>
2023-10-31 11:16:47 -0300

    Add flag for set randoops random seed

---
+++ src/main/java/randoop/main/randoopflags/RandomSeedFlag.java
@@ -0,0 +1,14 @@
+package randoop.main.randoopflags;
+
+public class RandomSeedFlag extends RandoopFlag{
+
+    int seed;
+    public RandomSeedFlag(int seed){
+        this.flagName = "randomseed";
+        this.seed = seed;
+    }
+    @Override
+    protected String flagParameterToString() {
+        return String.valueOf(seed);
+    }
+}
