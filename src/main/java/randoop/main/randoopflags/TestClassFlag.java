3d3a48e0ac20acddebf9bc6f153870e58555eec0
AFungo <augustofungo@gmail.com>
2023-10-31 11:11:15 -0300

    Change string attributte for Class attribute

--- src/main/java/randoop/main/randoopflags/TestClassFlag.java
+++ src/main/java/randoop/main/randoopflags/TestClassFlag.java
@@ -2,15 +2,15 @@ package randoop.main.randoopflags;
 
 public class TestClassFlag extends RandoopFlag {
 
-    String testClass;//maybe this can be Class<Object>, but i don't know how to include class package,
+    private Class<?> testClass;//maybe this can be Class<Object>, but i don't know how to include class package,
                     // can i put another attribute for package? I don't look it well
 
-    public TestClassFlag(String testClass){
+    public TestClassFlag(Class<?> testClass){
         this.flagName = "testclass";
         this.testClass = testClass;
     }
     @Override
     protected String flagParameterToString() {
-        return testClass;
+        return testClass.getName();
     }
 }
