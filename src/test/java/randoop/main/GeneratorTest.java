c6e264c0ff6972f67d457188fa701dcdf87cd4b9
AFungo <augustofungo@gmail.com>
2023-11-16 13:30:09 -0300

    Improved method source

--- src/test/java/randoop/main/GeneratorTest.java
+++ src/test/java/randoop/main/GeneratorTest.java
@@ -1,7 +1,6 @@
 package randoop.main;
 
 import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.assertj.core.api.Assertions;
@@ -22,24 +21,22 @@ public class GeneratorTest {
 
     @Test
     public void test() throws IOException {
-
         RandoopObjectGenerator rog = new RandoopObjectGenerator(java.util.Stack.class);//Poner la clase;
         rog.setSeed(100);//Con la semilla 100 la mayoria son iguales, son [coconut] o []
-//        rog.setRunTime(0);
-        rog.setOutputLimitFlag(20);
-//        Set<Object> list = new HashSet<>(rog.generateObjects());
+        rog.setOutputLimitFlag(10);
         List<Object> list = rog.generateObjects();
         for (Object o: list) {
             System.out.println(o);
         }
-//        assertThat(list, CoreMatchers.is(Matchers.hasSize((int) list.stream().distinct().count())));
+        assertThat(list.size(), CoreMatchers.is(10));
+
     }
 
     @ParameterizedTest
-    @MethodSource("amountGenerator")
-    public void objectAmountGenerationTest(int amount, Class<?> c){
+    @MethodSource("amountAndSeedGenerator")
+    public void objectAmountGenerationTest(int amount, Class<?> c, int seed){
         RandoopObjectGenerator rog = new RandoopObjectGenerator(c);//Poner la clase;
-        rog.setSeed(100);
+        rog.setSeed(seed);
         rog.setOutputLimitFlag(amount);
         assertThat(rog.generateObjects().size(), CoreMatchers.is(amount));
     }
@@ -51,37 +48,11 @@ public class GeneratorTest {
         rog.setSeed(seed);
         rog.setOutputLimitFlag(amount);
         List<Object> list = rog.generateObjects();
-        for (Object o: list) {
-            System.out.println(o);
-        }
         Assertions.assertThat(list).doesNotHaveDuplicates();
-    }
-
-    public static Stream<Arguments> amountGenerator() {
-        return Stream.of(
-                Arguments.of(1, java.util.Stack.class),
-                Arguments.of(2, java.util.Stack.class),
-                Arguments.of(3, java.util.Stack.class),
-                Arguments.of(4, java.util.Stack.class),
-                Arguments.of(5, java.util.Stack.class),
-                Arguments.of(10, java.util.Stack.class),
-                Arguments.of(15, java.util.Stack.class),
-                Arguments.of(20, java.util.Stack.class),
-                Arguments.of(1, java.util.Queue.class),
-                Arguments.of(2, java.util.Queue.class),
-                Arguments.of(3, java.util.Queue.class),
-                Arguments.of(4, java.util.Queue.class),
-                Arguments.of(5, java.util.Queue.class),
-                Arguments.of(10, java.util.Queue.class),
-                Arguments.of(15, java.util.Queue.class),
-                Arguments.of(20, java.util.Queue.class)
-                );
     }
 
     public static Stream<Arguments> amountAndSeedGenerator() {
         return Stream.of(
-                Arguments.of(10, java.util.Stack.class, 100),
-                Arguments.of(10, java.util.Queue.class, 100),
                 Arguments.of(10, java.util.Stack.class, 1),
                 Arguments.of(10, java.util.Queue.class, 1),
                 Arguments.of(10, java.util.Stack.class, 2),
@@ -95,7 +66,23 @@ public class GeneratorTest {
                 Arguments.of(10, java.util.Stack.class, 50),
                 Arguments.of(10, java.util.Queue.class, 50),
                 Arguments.of(10, java.util.Stack.class, 47),
-                Arguments.of(10, java.util.Queue.class, 47)
+                Arguments.of(10, java.util.Queue.class, 47),
+                Arguments.of(1, java.util.Stack.class, 100),
+                Arguments.of(2, java.util.Stack.class, 100),
+                Arguments.of(3, java.util.Stack.class, 100),
+                Arguments.of(4, java.util.Stack.class, 100),
+                Arguments.of(5, java.util.Stack.class, 100),
+                Arguments.of(10, java.util.Stack.class, 100),
+                Arguments.of(15, java.util.Stack.class, 100),
+                Arguments.of(20, java.util.Stack.class, 100),
+                Arguments.of(1, java.util.Queue.class, 100),
+                Arguments.of(2, java.util.Queue.class, 100),
+                Arguments.of(3, java.util.Queue.class, 100),
+                Arguments.of(4, java.util.Queue.class, 100),
+                Arguments.of(5, java.util.Queue.class, 100),
+                Arguments.of(10, java.util.Queue.class, 100),
+                Arguments.of(15, java.util.Queue.class, 100),
+                Arguments.of(20, java.util.Queue.class, 100)
         );
     }
 }
