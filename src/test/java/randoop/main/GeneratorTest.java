33741260b7c1f91fdd8965606f36e85d6d1bd5fd
AFungo <augustofungo@gmail.com>
2023-11-24 08:29:52 -0300

    Added test cases

--- src/test/java/randoop/main/GeneratorTest.java
+++ src/test/java/randoop/main/GeneratorTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,7 +22,7 @@ public class GeneratorTest {
     @Test
     public void test() throws IOException {
         RandoopObjectGenerator rog = new RandoopObjectGenerator(java.util.Stack.class);//Poner la clase;
-        rog.setSeed(100);//Con la semilla 100 la mayoria son iguales, son [coconut] o []
+        rog.setSeed(1);//Con la semilla 100 la mayoria son iguales, son [coconut] o []
         rog.setOutputLimitFlag(10);
         List<Object> list = rog.generateObjects();
         for (Object o: list) {
@@ -35,7 +35,7 @@ public class GeneratorTest {
     @ParameterizedTest
     @MethodSource("amountAndSeedGenerator")
     public void objectAmountGenerationTest(int amount, Class<?> c, int seed){
-        RandoopObjectGenerator rog = new RandoopObjectGenerator(c);//Poner la clase;
+        RandoopObjectGenerator rog = new RandoopObjectGenerator(c);
         rog.setSeed(seed);
         rog.setOutputLimitFlag(amount);
         assertThat(rog.generateObjects().size(), CoreMatchers.is(amount));
@@ -48,29 +48,86 @@ public class GeneratorTest {
         rog.setSeed(seed);
         rog.setOutputLimitFlag(amount);
         List<Object> list = rog.generateObjects();
+        for (Object o: list) {
+            System.out.println(o);
+        }
         Assertions.assertThat(list).doesNotHaveDuplicates();
     }
 
     public static Stream<Arguments> amountAndSeedGenerator() {
         return Stream.of(
                 Arguments.of(10, java.util.Stack.class, 1),
-                Arguments.of(10, java.util.Queue.class, 1),
                 Arguments.of(10, java.util.Stack.class, 10),
-                Arguments.of(10, java.util.Queue.class, 10),
                 Arguments.of(10, java.util.Stack.class, 99),
-                Arguments.of(10, java.util.Queue.class, 99),
                 Arguments.of(10, java.util.Stack.class, 20),
-                Arguments.of(10, java.util.Queue.class, 20),
                 Arguments.of(10, java.util.Stack.class, 47),
-                Arguments.of(10, java.util.Queue.class, 47),
                 Arguments.of(1, java.util.Stack.class, 100),
                 Arguments.of(10, java.util.Stack.class, 100),
                 Arguments.of(15, java.util.Stack.class, 100),
                 Arguments.of(20, java.util.Stack.class, 100),
-                Arguments.of(1, java.util.Queue.class, 100),
-                Arguments.of(10, java.util.Queue.class, 100),
-                Arguments.of(15, java.util.Queue.class, 100),
-                Arguments.of(20, java.util.Queue.class, 100)
+                Arguments.of(10, TreeSet.class, 1),
+                Arguments.of(10, TreeSet.class, 10),
+                Arguments.of(10, TreeSet.class, 99),
+                Arguments.of(10, TreeSet.class, 20),
+                Arguments.of(10, TreeSet.class, 47),
+                Arguments.of(1, TreeSet.class, 100),
+                Arguments.of(10, TreeSet.class, 100),
+                Arguments.of(15, TreeSet.class, 100),
+                Arguments.of(20, TreeSet.class, 100),
+                Arguments.of(10, PriorityQueue.class, 1),
+                Arguments.of(10, PriorityQueue.class, 10),
+                Arguments.of(10, PriorityQueue.class, 99),
+                Arguments.of(10, PriorityQueue.class, 20),
+                Arguments.of(10, PriorityQueue.class, 47),
+                Arguments.of(1, PriorityQueue.class, 100),
+                Arguments.of(10, PriorityQueue.class, 100),
+                Arguments.of(15, PriorityQueue.class, 100),
+                Arguments.of(20, PriorityQueue.class, 100),
+//                Arguments.of(10, OptionalDouble.class, 1),
+//                Arguments.of(10, OptionalDouble.class, 10),
+//                Arguments.of(10, OptionalDouble.class, 99),
+//                Arguments.of(10, OptionalDouble.class, 20),
+//                Arguments.of(10, OptionalDouble.class, 47),
+//                Arguments.of(1, OptionalDouble.class, 100),
+//                Arguments.of(10, OptionalDouble.class, 100),
+//                Arguments.of(15, OptionalDouble.class, 100),
+//                Arguments.of(20, OptionalDouble.class, 100),
+                Arguments.of(10, Locale.class, 1),
+                Arguments.of(10, Locale.class, 10),
+                Arguments.of(10, Locale.class, 99),
+                Arguments.of(10, Locale.class, 20),
+                Arguments.of(10, Locale.class, 47),
+                Arguments.of(1, Locale.class, 100),
+                Arguments.of(10, Locale.class, 100),
+                Arguments.of(15, Locale.class, 100),
+                Arguments.of(20, Locale.class, 100),
+                Arguments.of(10, HashMap.class, 1),
+                Arguments.of(10, HashMap.class, 10),
+                Arguments.of(10, HashMap.class, 99),
+                Arguments.of(10, HashMap.class, 20),
+                Arguments.of(10, HashMap.class, 47),
+                Arguments.of(1, HashMap.class, 100),
+                Arguments.of(10, HashMap.class, 100),
+                Arguments.of(15, HashMap.class, 100),
+                Arguments.of(20, HashMap.class, 100),
+                Arguments.of(10, BitSet.class, 1),
+                Arguments.of(10, BitSet.class, 10),
+                Arguments.of(10, BitSet.class, 99),
+                Arguments.of(10, BitSet.class, 20),
+                Arguments.of(10, BitSet.class, 47),
+                Arguments.of(1, BitSet.class, 100),
+                Arguments.of(10, BitSet.class, 100),
+                Arguments.of(15, BitSet.class, 100),
+                Arguments.of(20, BitSet.class, 100),
+                Arguments.of(10, Date.class, 1),
+                Arguments.of(10, Date.class, 10),
+                Arguments.of(10, Date.class, 99),
+                Arguments.of(10, Date.class, 20),
+                Arguments.of(10, Date.class, 47),
+                Arguments.of(1, Date.class, 100),
+                Arguments.of(10, Date.class, 100),
+                Arguments.of(15, Date.class, 100),
+                Arguments.of(20, Date.class, 100)
         );
     }
 }
