67446576c1c0d825d19d001dcbbc4f18ee32557a
AFungo <augustofungo@gmail.com>
2024-07-12 12:37:52 -0300

    Set omit-methods in  Pilas's test

--- src/test/java/randoop/main/DataStructureTest.java
+++ src/test/java/randoop/main/DataStructureTest.java
@@ -1,13 +1,10 @@
 package randoop.main;
 
 import examples.datastructure.PilaSobreListasEnlazadas;
-import examples.datastructure.Stack;
-import java7.util7.Arrays;
 import org.junit.jupiter.api.Test;
 import randoop.sequence.Sequence;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class DataStructureTest {
@@ -15,6 +12,7 @@ public class DataStructureTest {
     @Test
     public void printObjectAndSequenceTest(){
         RandoopObjectGenerator rog = new RandoopObjectGenerator(PilaSobreListasEnlazadas.class, 10);
+        rog.setOmitMethods("isEmpty|length|top|toList|toString|equals|hash");
         for (int i = 0; i < 10; i++) {
             Object o = rog.generateOneObject();
             Set<Sequence> l = rog.getSequences();
@@ -27,9 +25,11 @@ public class DataStructureTest {
     @Test
     public void printObjectTest(){
         RandoopObjectGenerator rog = new RandoopObjectGenerator(PilaSobreListasEnlazadas.class, 231);
-        for (int i = 0; i < 5; i++) {
-            Object o = rog.generateOneObject();
-            System.out.println(o);
+        rog.setOmitMethods("isEmpty|length|top|toList|toString|equals|hash");
+        rog.setIntegerRange(140, 150);
+        for (int i = 0; i < 100; i++) {
+            PilaSobreListasEnlazadas o = (PilaSobreListasEnlazadas) rog.generateOneObject();
+            System.out.println(o + " - size = " +  o.length());
         }
     }
 }
