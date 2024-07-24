d1ae5ebbf6d5528ec7f462d98b23e1631e5aab3d
AFungo <augustofungo@gmail.com>
2024-07-24 13:37:23 -0300

    Implement example of tuples and add new test for the new feature

--- src/test/java/randoop/main/DataStructureTest.java
+++ src/test/java/randoop/main/DataStructureTest.java
@@ -1,11 +1,15 @@
 package randoop.main;
 
 import examples.datastructure.PilaSobreListasEnlazadas;
+import examples.datastructure.PilasTuple;
 import org.junit.jupiter.api.Test;
 import randoop.sequence.Sequence;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DataStructureTest {
 
@@ -26,10 +30,25 @@ public class DataStructureTest {
     public void printObjectTest(){
         RandoopObjectGenerator rog = new RandoopObjectGenerator(PilaSobreListasEnlazadas.class, 231);
         rog.setOmitMethods("isEmpty|length|top|toList|toString|equals|hash");
-        rog.setIntegerRange(140, 150);
+        Set<Integer> integers = IntStream.range(110, 120).boxed().collect(Collectors.toSet());
+        rog.setCustomIntegers(integers);
         for (int i = 0; i < 100; i++) {
             PilaSobreListasEnlazadas o = (PilaSobreListasEnlazadas) rog.generate();
             System.out.println(o + " - size = " +  o.length());
+        }
+    }
+
+    @Test
+    public void pilasTupleTest(){
+        RandoopObjectGenerator rog = new RandoopObjectGenerator(PilasTuple.class, 231);
+        rog.setOmitMethods("toString|equals|hash");
+        Set<Integer> integers = IntStream.range(110, 120).boxed().collect(Collectors.toSet());
+        rog.setCustomIntegers(integers);
+        rog.setNecessaryClasses(Collections.singleton(PilaSobreListasEnlazadas.class));
+        for (int i = 0; i < 100; i++) {
+            PilasTuple o = (PilasTuple) rog.generate();
+            Set<Sequence> seq = rog.getSequences();
+            System.out.println("----------\n" + o + "\n" + new ArrayList<>(seq).get(seq.size()-1) + "\n----------\n");
         }
     }
 }
